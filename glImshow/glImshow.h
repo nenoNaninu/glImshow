804f387d7729c20c9cbfc4d462183f1dfdd40cc6
nenoNaninu <neno@neno-garden.com>
2018-03-10 01:13:43 +0900

    いろいろ書いたﾝｺﾞ

---
+++ glImshow/glImshow.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "stdafx.h"
+
+void glImshowInit(int argc, char *argv[], int width, int height, const char* windowName, int glutMode);
+void glImshowStart();
+void glImshowSetFunc(void(*display)(void), void(*keydordFunc)(unsigned char, int, int), void(*mainLoop)(void));
+void glImshow(cv::Mat& mat, int convertFormatCV2GL, int glShowFormat);
---
+++ glImshow/main.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include "glImshow.h"
+
+void display();
+void keyboard(unsigned char key, int x, int y);
+void mainLoop();
+
+cv::VideoCapture capture(0);
+
+int main(int argc, char *argv[])
+{
+	glImshowInit(argc, argv, capture.get(CV_CAP_PROP_FRAME_WIDTH), capture.get(CV_CAP_PROP_FRAME_HEIGHT), "glImshow", GLUT_RGB);
+	glImshowSetFunc(display, keyboard, mainLoop);
+	glImshowStart();
+	return 0;
+}
+
+void display()
+{
+
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'q':
+	case 'Q':
+	case '\033':
+		glutLeaveMainLoop();
+	default:
+		break;
+	}
+}
+
+void mainLoop()
+{
+	cv::Mat mat;
+	capture >> mat;
+	glImshow(mat, CV_BGR2RGB, GL_RGB);
+}
