ce6ecd3af22707ad06efdb0e58c34e3b0c5a434c
nenoNaninu <neno@neno-garden.com>
2018-05-13 00:21:40 +0900

    マウスの操作追加

--- glImshow/glImshow.h
+++ glImshow/glImshow.h
@@ -3,5 +3,5 @@
 
 void glImshowInit(int argc, char *argv[], int width, int height, const char* windowName, int glutMode);
 void glImshowStart();
-void glImshowSetFunc(void(*display)(void), void(*keydordFunc)(unsigned char, int, int), void(*mainLoop)(void));
+void glImshowSetFunc(void(*display)(void), void(*keydordFunc)(unsigned char, int, int), void(*mainLoop)(void), void(*mousePressed)(int button, int state, int x, int y));
 void glImshow(const cv::Mat& mat, int convertFormatCV2GL, int glShowFormat);
--- glImshow/main.cpp
+++ glImshow/main.cpp
@@ -4,13 +4,14 @@
 void display();
 void keyboard(unsigned char key, int x, int y);
 void mainLoop();
+void mousePressed(int button, int state, int x, int y);
 
 cv::VideoCapture capture(0);
 
 int main(int argc, char *argv[])
 {
 	glImshowInit(argc, argv, capture.get(CV_CAP_PROP_FRAME_WIDTH), capture.get(CV_CAP_PROP_FRAME_HEIGHT), "glImshow", GLUT_RGB);
-	glImshowSetFunc(display, keyboard, mainLoop);
+	glImshowSetFunc(display, keyboard, mainLoop,mousePressed);
 	glImshowStart();
 	return 0;
 }
@@ -38,4 +39,9 @@ void mainLoop()
 	cv::Mat mat;
 	capture >> mat;
 	glImshow(mat, CV_BGR2RGB, GL_RGB);
+}
+
+void mousePressed(int button, int state, int x, int y)
+{
+	std::cout << x << " " << y << std::endl;
 }
