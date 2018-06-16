b03e27a3df9cdce7d250a66136a04624a2b9ef51
nenoNaninu <neno@neno-garden.com>
2018-06-16 12:07:41 +0900

    doubleBuffer

--- glImshow/main.cpp
+++ glImshow/main.cpp
@@ -5,6 +5,7 @@ void display();
 void keyboard(unsigned char key, int x, int y);
 void mainLoop();
 void mousePressed(int button, int state, int x, int y);
+void displayFps();
 
 cv::VideoCapture capture(0);
 
@@ -39,9 +40,23 @@ void mainLoop()
     cv::Mat mat;
     capture >> mat;
     glImshow(mat, CV_BGR2RGB, GL_RGB);
+    displayFps();
 }
 
 void mousePressed(int button, int state, int x, int y)
 {
     std::cout << x << " " << y << std::endl;
+}
+
+void displayFps()
+{
+    static int fps = 0;
+    static std::chrono::system_clock::time_point time = std::chrono::system_clock::now();
+    if (std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now() - time).count() >= 1000)
+    {
+        std::cout << fps << std::endl;
+        fps = 0;
+        time = std::chrono::system_clock::now();
+    }
+    fps++;
 }
